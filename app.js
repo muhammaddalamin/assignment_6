d3412943868c5e80fabfae09869f487814f1b329
iqbalhossain <iqbalhossainsha2303@gmail.com>
2021-02-14 17:09:54 +0600

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
+// two new features(bonus content)
+
+// current location
+// window.onload = function () {
+//   navigator.geolocation.getCurrentPosition(success => {
+//     console.log(success);
+//     document.getElementById('location').innerHTML = success
+//   }, error => {
+//     console.log(error);
+//   })
+// }
+
+
+// current location 
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+// full window loading
+window.onload = function () {
+  document.getElementById('container').textContent = body;
+}
+
+
 const imagesArea = document.querySelector('.images');
 const gallery = document.querySelector('.gallery');
 const galleryHeader = document.querySelector('.gallery-header');
@@ -11,7 +62,7 @@ let sliders = [];
 // If this key doesn't work
 // Find the name in the url and go to their website
 // to create your own api key
-const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+const KEY = '20264732-0a80be9cd0a9ad640977dda6d';
 
 // show images 
 const showImages = (images) => {
@@ -21,30 +72,34 @@ const showImages = (images) => {
   galleryHeader.style.display = 'flex';
   images.forEach(image => {
     let div = document.createElement('div');
-    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2  ';
+    div.innerHTML = ` <img class="img-fluid img-thumbnail " id="" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
   })
 
 }
 
+
 const getImages = (query) => {
-  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+  fetch(`https://pixabay.com/api/?key=${KEY}=yellow+flowers&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
- 
+
+  element.classList.toggle('added');
+
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    // alert('Hey, Already added !')
+    element.classList.add('remove');
   }
 }
 var timer
@@ -67,7 +122,10 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
+
+
+  const duration = (document.getElementById('duration').value <= 0) ? 1000 : document.getElementById('duration').value;
+
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
@@ -77,6 +135,7 @@ const createSlider = () => {
     sliderContainer.appendChild(item)
   })
   changeSlide(0)
+
   timer = setInterval(function () {
     slideIndex++;
     changeSlide(slideIndex);
@@ -120,3 +179,25 @@ searchBtn.addEventListener('click', function () {
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
+
+
+// keyPress listener
+// let searchBtn = document.getElementById("search-btn");
+let searchField = document.getElementById("search");
+
+searchField.addEventListener("keypress", function (event) {
+
+  if (event.key == 'Enter') {
+    document.getElementById('search-btn').click();
+  }
+
+});
+
+
+
+
+
+
+
+
+
